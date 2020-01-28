edb510e6525e5c0365d07730b7fea7d05d3b2074
Richard <richardcarsongill@gmail.com>
2020-01-27 19:15:43 -0500

    Create exponet function

--- src/power.js
+++ src/power.js
@@ -12,6 +12,20 @@
 // power(5, 2) => 25
 // power(10, 3) => 1000
 
+
+let placeStuff = 1
 var power = function(base, exponent) {
-  // Your code here
-};
+    // create if else statement and put in recursive function call
+    // set condition to run function again if true
+    if (exponent > 0) {
+        placeStuff = placeStuff * base
+    
+      return power(base, exponent -1)
+    } else {
+      let returnValue = placeStuff
+      placeStuff = 1
+      return returnValue
+    }
+  
+  };
+
